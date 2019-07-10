ef72bdd6bcea8c0f0d792a9fb8203f866ec943d4
Pawid-prianto27 <pawitanto27@gmail.com>
2019-07-10 21:17:10 +0700

    bismilahirohmannirohim..

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import "./Footer.scss";
+export default class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Copyright&copy; 2019 design&amp;code with <i class="fab fa-gratipay"></i></h2>
+      </div>
+    )
+  }
+}
---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    kota: "Jogja",
+    img: "./img/jogja.jpg",
+    nama: "Jogja Istimewa",
+    info:
+    "Liburan dijogja dengan keluarga"
+  },
+  {
+    id: 2,
+    kota: "Magelang",
+    img: "./img/temple1.jpg",
+    nama: " Berburu sunset Borobudur",
+    info:
+    "Santai di Borobudur"
+  },
+  {
+    id: 3,
+    kota: "ParangTritis",
+    img: "./img/beach.jpg",
+    nama: "ParangTritis Romantis",
+    info:
+    "Santai dipantai"
+  },
+  {
+    id: 4,
+    kota: "Kebun Binatang",
+    img: "./img/bonbin.jpg",
+    nama: "Taman Raya",
+    info:
+   "BonBin Menantang..."
+  },
+];
